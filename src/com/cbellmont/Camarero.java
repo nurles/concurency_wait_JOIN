f00f8ddf0115e5555eba299220d0ff50930bf3bf
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2020-10-27 13:20:08 +0100

    Fixed

--- src/com/cbellmont/Camarero.java
+++ src/com/cbellmont/Camarero.java
@@ -8,18 +8,25 @@ public class Camarero extends Thread {
             while (true) {
                 synchronized (Main.list){
                     Main.list.wait();
-                    System.out.println("Camarero - Si he llegado aqui es porque el cliente ha pedido comida. Debo llevarla");
-                    Main.list.add("Zumo");
-                    Main.list.add("Tostada");
-                    Main.list.add("Mermelada");
-                    System.out.println("Camarero - El pedido ya esta listo. Tardo 5 segundos en llevarle la comida al cliente");
-                    sleep(5000);
+                    prepararComanda();
                     Main.list.notify();
                 }
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private void prepararComanda() throws InterruptedException {
+        System.out.println("Camarero - Si he llegado aqui es porque el cliente ha pedido comida. Debo llevarla");
+        Main.list.add("Zumo");
+        sleep(1000);
+        Main.list.add("Tostada");
+        sleep(1000);
+        Main.list.add("Mermelada");
+        sleep(1000);
+        System.out.println("Camarero - El pedido ya esta listo. Tardo 5 segundos en llevarle la comida al cliente");
+        sleep(5000);
     }
 }
 
--- src/com/cbellmont/Main.java
+++ src/com/cbellmont/Main.java
@@ -9,7 +9,10 @@ public class Main {
     public static void main(String[] args) {
         Cliente cliente = new Cliente();
         Camarero camarero = new Camarero();
+        // Es importante que empiece el camarero ya que se queda esperando.
         camarero.start();
         cliente.start();
     }
+
+
 }
